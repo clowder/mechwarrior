fe4910555d1da71908dcf82e0eba2ff8b6a3ea71
mcrmfc <matthew.robbins@bbc.co.uk>
2011-11-22 17:19:24 +0000

    allows us to handle a Google search scenario i.e. a GET request on a form with a relative action - where mechanize needs the full URL not just the action

--- lib/capybara/mechanize/form.rb
+++ lib/capybara/mechanize/form.rb
@@ -1,33 +1,32 @@
 require 'mechanize'
 
 class Capybara::Mechanize::Form < Mechanize::Form
-  
+
   def initialize(driver, form_node)
     @driver   = driver
     form_node = form_node.clone
-    
+
     form_node.search('*[disabled=disabled]').remove
-    
+
     super(form_node, @driver.browser.agent)
   end
-  
+
   def submit(button_node)
     button        = Mechanize::Form::Button.new(button_node)
-    self.action ||= @driver.current_url
-    
+    resolve_action(@driver.current_url)
+
     self.normalize_uploads!
     self.add_button_to_query(button)
-
     @driver.submit(self)
   end
-  
+
   protected
-    
+
   def normalize_uploads!
     self.file_uploads.each do |upload|
       file_path = upload.node["value"]
       next if file_path.nil? || file_path == ''
-      
+
       if self.multipart?
         upload.file_name = file_path 
       else
@@ -36,8 +35,19 @@ class Capybara::Mechanize::Form < Mechanize::Form
       end
     end
   end
-  
+
   def multipart?
     !!(self.enctype.downcase =~ /^multipart\/form-data/)
   end  
+
+  private
+
+  def resolve_action(current_url)
+    if self.method.upcase == 'GET' and self.action !~ /^http/ and self.action.to_s != ""
+      uri = URI.parse(current_url)
+      self.action = "#{uri.scheme}://#{uri.host}:#{uri.port}#{self.action}"
+    else
+      self.action ||= current_url
+    end
+  end
 end
